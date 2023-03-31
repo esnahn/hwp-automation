f8108f12e21f219c2bb4d7436d1a2140b62b0aed
Euisoon Ahn <euisoon.ahn@gmail.com>
2023-03-31 16:50:47 +0900

    change reports path

--- pdf.py
+++ pdf.py
@@ -2,7 +2,7 @@ from time import sleep
 
 from win32hwp import get_hwp_object, get_hwp_paths, hwp_save_as_pdf
 
-parent = "C:\\Users\\USER\Documents\\auri 연구보고서"
+parent = "D:\\auri 연구보고서"
 hwppaths = get_hwp_paths(parent)
 
 badfiles = ["[AURI-기본-2013-3] 가로단위 공간관리 수단으로서의 특별가로구역 제도 연구.hwp"]
